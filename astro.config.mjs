013172fbae3c7f258787c6a17f5ee3b8a064d094
noepm <pompeyonoe@gmail.com>
2023-08-12 22:02:20 -0600

    [ADD] rss y sitemap , con metadatos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from "@astrojs/react";
-
+import sitemap from '@astrojs/sitemap'
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue()]
+  site : 'https://astrobuild.tips',
+  integrations: [tailwind(), mdx(), react(), vue(), sitemap()]
 });
